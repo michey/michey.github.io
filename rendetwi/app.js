87eb577a3fae0d9c22f3c99e184ca6e816927a1b
Alexander Mihailov <mixeyy@gmail.com>
2015-08-23 23:07:58 +0300

    crap

--- rendetwi/app.js
+++ rendetwi/app.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById('renderer-port');
 var ctx = canvas.getContext('2d');
 
-var html = '<div>' +
-    '<span style=" font-size: 55px;">"&nbsp;</span>' +
-    '<span style=" font-size: 30px;">' +
+var block = '<div xmlns="http://www.w3.org/1999/xhtml">' +
+    '<span style="font-size: 55px;">"&nbsp;</span>' +
+    '<span style="font-size: 30px;">' +
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec purus in ante pretium blandit. Aliquam erat volutpat. Nulla libero lectus.' +
     '</span>' +
     '<div style="font-size: 55px; padding-left: 480px;">"</div>' +
@@ -11,20 +11,17 @@ var html = '<div>' +
     '<div>@Sandy_mustache</div>' +
     '</div>';
 
-var doc = document.implementation.createHTMLDocument("");
-doc.write(html);
-doc.documentElement.setAttribute("xmlns", doc.documentElement.namespaceURI);
-html = (new XMLSerializer).serializeToString(doc);
+var html = '<svg xmlns="http://www.w3.org/2000/svg" width="500" height="300">' +
+    '<foreignObject width="100%" height="100%">' + block +
+    '</foreignObject>' +
+    '</svg>';
 
 var DOMURL = window.URL || window.webkitURL || window;
 
+console.log(html);
 var img = new Image();
 var svg = new Blob([html], {type: 'image/svg+xml;charset=utf-8'});
 var url = DOMURL.createObjectURL(svg);
-
-console.log(html);
-console.log(img);
-console.log(svg);
 
 img.onload = function() {
   ctx.drawImage(img, 0, 0);
