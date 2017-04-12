b578fb93804d3f688c455fb49891d0b9ca6385e2
michey <mixeyy@gmail.com>
2017-04-12 23:52:16 +0300

    l

--- projects/vk/get-photos/index.js
+++ projects/vk/get-photos/index.js
@@ -1,17 +1,41 @@
-
 var albums = [];
+var photos = {};
 
 var loadAlbumsList = function () {
 
   VK.api("photos.getAlbums", function (data) {
     console.log(albums);
-    albums = data["response"];
+    albums = data["response"]["items"];
     loadPhotos();
+  })
+};
+
+
+var photoListingComplete = function () {
+  console.log(photos);
+};
+
+var handleResponse = function (data, albumId) {
+  var items = data["response"]["items"];
+  photos[albumId] = items.map(function (ph) {
+    return ph["photo_2560"];
+  });
+  if (Object.keys(photos).length === albums.length) {
+    photoListingComplete();
+  }
+
+};
+
+var getPhoto = function (albumId) {
+  VK.api("photos.get", {"album_id": albumId}, function (data) {
+    handleResponse(data, albumId);
   })
 };
 
 var loadPhotos = function () {
   albums.forEach(function (album) {
+    var id = album["id"];
+    getPhoto(id);
     console.log(album)
   })
 };
