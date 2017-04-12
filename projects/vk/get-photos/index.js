23781565a45017afeb38f622d5db03a6592f3baf
michey <mixeyy@gmail.com>
2017-04-12 23:11:51 +0300

    m

--- projects/vk/get-photos/index.js
+++ projects/vk/get-photos/index.js
@@ -1,11 +1,25 @@
+
+var id = 0;
+var albums = [];
+
 var getId = function() {
   VK.api("users.get", function (data) {
-    console.log(data);
+    id = data["response"][0]["id"];
+    loadAlbumsList();
   })
 };
 
 var loadAlbumsList = function () {
+  VK.api("photos.getAlbums", {"owner_id": id}, function (data) {
+    albums = data["response"];
+    loadPhotos();
+  })
+};
 
+var loadPhotos = function () {
+  albums.forEach(function (album) {
+    console.log(album)
+  })
 };
 
 var success = function () {
